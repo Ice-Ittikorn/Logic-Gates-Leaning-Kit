1b105a37fa4f3349733cb84a5446f0d28a6e0451
Ice-Ittikorn <67030261@kmitl.ac.th>
2026-02-05 21:58:04 +0700

    Remove Dockerfile and test files; implement GPIO setup and LCD initialization in main application

--- production/main/main.c
+++ production/main/main.c
@@ -1,15 +1,39 @@
+#include <stdio.h>
 #include "logic_gate.h"
 #include "esp_log.h"
 #include "lcd_i2c.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+// ===== GPIO =====
+#define LED_R GPIO_NUM_25
+#define LED_G GPIO_NUM_26
+#define LED_B GPIO_NUM_27
+
+void setup (void)
+{
+    // GPIO
+    gpio_reset_pin(LED_R); // R
+    gpio_reset_pin(LED_G); // G
+    gpio_reset_pin(LED_B); // B
+    gpio_set_direction(LED_R, GPIO_MODE_OUTPUT); 
+    gpio_set_direction(LED_G, GPIO_MODE_OUTPUT);
+    gpio_set_direction(LED_B, GPIO_MODE_OUTPUT);
+
+    // LCD
+    lcd_init();
+    Lcd_print(0, 0, " ");
+    Lcd_print(0, 1, " ");
+    VTaskDelay(500 / portTICK_PERIOD_MS);
+    Lcd_print(1, 0, "E Logic Board");
+    Lcd_print(0, 1, "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"); 
+    vTaskDelay(pdMS_TO_TICKS(500));
+}
+
 
 void app_main(void)
 {
-    lcd_init();
-
-    // เหมือน Arduino:
-    // lcd.setCursor(0,0); lcd.print("Hello");
-    Lcd_print(0, 0, "Hello ESP32");
-
-    // lcd.setCursor(0,1); lcd.print("I2C LCD");
-    Lcd_print(0, 1, "I2C LCD");
+    setup();
 }
+
